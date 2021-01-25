2c9b385b3f27b6664ff186bc55715d181498aecb
Opilane TTHK <opilane@tthk.ee>
2021-01-25 13:59:44 +0200

    first commit

---
+++ CatsAndDogs/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace CatsAndDogs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.getDog = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // getDog
+            // 
+            this.getDog.Location = new System.Drawing.Point(581, 357);
+            this.getDog.Name = "getDog";
+            this.getDog.Size = new System.Drawing.Size(75, 23);
+            this.getDog.TabIndex = 0;
+            this.getDog.Text = "GET DOG\r\n\r\n\r\n";
+            this.getDog.UseVisualStyleBackColor = true;
+            this.getDog.Click += new System.EventHandler(this.getDog_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(434, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(354, 310);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.getDog);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button getDog;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ CatsAndDogs/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CatsAndDogs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void getDog_Click(object sender, EventArgs e)
+        {
+            
+        }
+        public static string getDogUrl()
+        {
+            string url = "https://dog.ceo/api/breeds/image/random";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+            string imageURL;
+
+            using (var responsreReader = new StreamReader(webStream))
+            {
+                var response = responsreReader.ReadToEnd();
+
+
+            }
+        }
+            
+       
+    }
+}
